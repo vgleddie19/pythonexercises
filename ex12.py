a2344ad41bb13111c022d2a134a20b8b85845245
Eddie <ihubeddie19@gmail.com>
2019-03-06 22:22:43 +0800

    Exercise 12 Done

---
+++ ex12.py
@@ -0,0 +1,6 @@
+name = raw_input("Name? ")
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weight? ")
+
+print "Hi %r So you're %r years old, %r inches tall and %r heavy." %(name,age,height,weight)
