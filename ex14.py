6cc29cdcaf22b3d635548910d1898ec30a0881a4
Eddie <ihubeddie19@gmail.com>
2019-03-09 13:12:43 +0800

    New prompt Value

--- ex14.py
+++ ex14.py
@@ -1,7 +1,8 @@
 from sys import argv
 
 script, user_name = argv
-prompt = '> '
+#prompt = '> '
+prompt = '======> '
 
 print "Hi %s, I'm the %s script." % (user_name, script)
 print "I'd like to ask you a few questions."
